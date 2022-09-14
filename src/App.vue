4cc7833966c422adae65086649aa2ff5170f829e
lingongbing <462441355@qq.com>
2022-09-14 11:37:39 +0800

    脸标志检测

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div id="nav">
+      <ul>
+        <li v-for="(item, index) in $router.options.routes" :key="index">
+          <router-link :to="item.path" v-text="item.name"></router-link>
+        </li>
+      </ul>
+    </div>
+    <div id="content">
+      <router-view />
+    </div>
   </div>
 </template>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  display: flex;
+  flex-direction: row;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+#nav {
+  min-width: 200px;
+  border: 2px #840484 solid;
+  padding: 10px;
+  margin-right: 20px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#nav ul,
+#nav li {
+  list-style: none;
+  margin: 10px 0;
+  padding: 0;
+  line-height: 30px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #140377;
+}
+
+#content {
+  flex-grow: 1;
+  border: 2px #c00934 solid;
+  padding: 20px;
 }
 </style>
